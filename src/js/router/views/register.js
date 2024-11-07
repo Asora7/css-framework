b14bc337e6053b6241fa3de2e1a100ec7353648b
Asora7 <verstr01474@stud.noroff.no>
2024-11-07 17:29:57 +0100

    Fix: Export onRegister function

--- src/js/router/views/register.js
+++ src/js/router/views/register.js
@@ -4,8 +4,7 @@
  * This module manages the user registration view. It sets up an event listener
  * for the registration form and handles user registration upon form submission.
  */
-
-import { onRegister } from '../../ui/auth/register';
+// import { onRegister } from '../../ui/auth/register';
 
 const form = document.forms.register;
 
