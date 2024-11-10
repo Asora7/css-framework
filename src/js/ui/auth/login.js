6eb6bbcaddc2276ff92d85d7fb8a075ae0245282
Asora7 <verstr01474@stud.noroff.no>
2024-11-10 13:01:32 +0100

    reversing login form

--- src/js/ui/auth/login.js
+++ src/js/ui/auth/login.js
@@ -7,27 +7,22 @@ import { loginUser } from '../../api/auth/login.js';
  * @param {Event} event - The event object representing the form submission event.
  * @returns {Promise<void>} A promise that resolves when the login process is complete.
  */
-export async function onLogin(event) {
-  event.preventDefault();  // Prevent the form from being submitted the default way (GET request)
 
-  const loginForm = document.forms['login'];  // Get the form
-  const formData = new FormData(loginForm);  // Extract form data
+export async function onLogin(event) {
+  event.preventDefault();
+
+  const loginForm = document.forms['login'];
+  const formData = new FormData(loginForm);
 
   const userData = {
     email: formData.get('email'),
     password: formData.get('password'),
   };
 
-  try {
-    await loginUser(userData);  // Call the loginUser function from the API
-  } catch (error) {
-    console.error("Login error:", error);
-    alert(error.message);  // Optionally display an error message to the user
-  }
+  await loginUser(userData);
 }
 
-// Get the login form element and add the event listener
 const loginForm = document.forms['login'];
 if (loginForm) {
-  loginForm.addEventListener('submit', onLogin);  // Trigger onLogin when form is submitted
+  loginForm.addEventListener('submit', onLogin);
 }
